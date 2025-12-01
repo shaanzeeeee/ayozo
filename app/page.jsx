2287a51190ec491481cf1f169385fea40b6b1541
shaanzeeeee <mahinuzzaman.shaan@gmail.com>
2025-12-01 21:07:39 +0600

    Landing page creation

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <div>
+    <section>
+      <div>
+        {/*left content*/}
+          <div>
+          <span className="text-gray-500 font-light tracking-wide mb-6">
+            AYOZO <span className="text-purple-400"> </span>
+
+          </span>
+
+
+
+          </div>
+
+        {/*right content*/}
+
+
+      </div>
+    </section>
+    
+        </div>
+}
