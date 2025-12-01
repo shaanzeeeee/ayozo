d070371f31f1b77edd69cded083d9ff5e3b0e5c2
shaanzeeeee <mahinuzzaman.shaan@gmail.com>
2025-12-01 20:33:31 +0600

    updated ticket system schema

--- convex/schema.js
+++ convex/schema.js
@@ -1,5 +1,7 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
+import { Tags } from "lucide-react";
+import { register } from "next/dist/next-devtools/userspace/pages/pages-dev-overlay-setup";
 export default defineSchema({
     //Users table
     users:defineTable({
@@ -28,4 +30,70 @@ export default defineSchema({
     createdAt:v.number(), 
     updatedAt:v.number(),
     }).index("by_token",["tokenIdentifier"]),
+
+    events:defineTable({
+        title:v.string(),
+        description:v.string(),
+        slug:v.string(),
+
+        //organizer
+        organizerId:v.id("users"), //userId of organizer
+        organizerName:v.string(),
+        //event details
+        category:v.string(),
+        tags:v.array(v.string()),
+
+        //date and time
+        startDate:v.number(),
+        endDate:v.number(),
+        timezone:v.string(),
+        //locations
+        locationType:v.union(v.literal("physical"),v.literal("online")), //online or in-person
+        venue:v.optional(v.string()),
+        address:v.optional(v.string()),
+        city:v.string(),
+        state: v.optional(v.string()),
+        //capacity
+        capacity:v.numbers(),
+        ticketType: v.union(v.literal("free"),v.literal("paid")),
+        ticketPrice:v.optional(v.number()),
+        registrationCount:v.number(),
+
+        //customization
+        coverImage:v.optional(v.string()),
+        themeColor:v.optional(v.string()),
+        //timetamps
+        createdAt:v.number(),
+        updatedAt:v.number(),
+
+
+    })
+    .index("by_organizer",["organizerId"])
+    .index("by_category",["category"])
+    .index("by_start_date",["startDate"])
+    .index("by_slug",["slug"])
+    .searchIndex("search_title",{searchField: "title"}),
+    registrations:defineTable({
+        eventId:v.id("events"),
+        userId:v.id("users"),
+        //attendee details
+        attendeeName:v.string(),
+        attendeeEmail:v.string(),
+
+        //QR code for ticket
+        qrCode:v.string(),
+        //CheckIn status
+        checkedIn:v.boolean(),
+        checkInAt:v.optional(v.number()),
+        //Status
+
+        status: v.union(v.literal("confirmed"),v.literal("cancelled")),
+        registeredAt:v.number(),
+    })
+    .index("by_event",["eventId"])
+    .index("by_user",["userId"])
+    .index("by_event_user",["eventId","userId"])
+    .index("by_qr_code",["qrCode"]),
+    
+
 });
